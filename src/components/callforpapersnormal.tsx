9679e380759525d1a1784840577df9c1ede175eb
Rekha <stackgurusolutions@gmail.com>
2026-03-02 10:06:36 +0530

    update

--- src/components/callforpapersnormal.tsx
+++ src/components/callforpapersnormal.tsx
@@ -80,7 +80,7 @@ export default function PaperSubmission() {
           <div className="flex justify-center mt-6 sm:mt-8">
           <button
             className="px-6 sm:px-8 py-2 sm:py-3 bg-indigo-600 text-white font-medium rounded-md hover:bg-indigo-700 transition-colors text-base sm:text-lg"
-            onClick={() => window.open("https://cmt3.research.microsoft.com/", "_blank")}
+            onClick={() => window.open("https://cmt3.research.microsoft.com/INDISCON2026/Submission/Index", "_blank")}
           >
             Submit Paper
           </button>
